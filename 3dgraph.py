aea3ef2fc814815cda34ab7d4b06fed0931766d4
d-monteiro <duartemsm26@gmail.com>
2024-02-14 12:15:56 +0000

    range modified

--- 3dgraph.py
+++ 3dgraph.py
@@ -6,8 +6,8 @@ from matplotlib import cm
 plt.style.use('_mpl-gallery')
 
 # Make data
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
+X = np.arange(-10, 10, 0.25)
+Y = np.arange(-10, 10, 0.25)
 X, Y = np.meshgrid(X, Y)
 R = np.sqrt(X**2 + Y**2)
 Z = np.cos(R)
