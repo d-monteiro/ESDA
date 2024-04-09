28c79c96753a91fecefa2fd3b6a5863c87169951
d-monteiro <duartemsm26@gmail.com>
2024-04-09 22:38:41 +0100

    added pl8

--- p08/ex01/f1.cpp
+++ p08/ex01/f1.cpp
@@ -6,15 +6,26 @@ using namespace std;
 
 int adicionar(priority_queue<string, vector<string>, greater<string> > *pq, string file_name)
 {
-  // alínea b)
+    
+    if(pq == nullptr || file_name.empty()) return -1;
+    
+    ifstream file(file_name);
+    if(!file) return -1;
 
-  return -1;
+    string line;
+
+    while(getline(file,line)) pq->push(line);
+    
+    return pq->size();
 }
 
 void imprimir(priority_queue<string, vector<string>, greater<string> > *pq, int n)
 {
-  // alínea c)
-  return;
+    cout<<"Primeiros 10 pilotos:\n";
+    for(int i=1; i<=n; i++){
+        cout<<"#"<<i<<": "<<pq->top()<<"\n";
+        pq->pop();
+    }
 }
 
 
